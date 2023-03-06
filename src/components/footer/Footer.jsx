e384482674bf54f05accf335aac43aeeeb42e242
SoniaGrassia <grassiasonia@gmail.com>
2023-03-06 19:41:31 +0100

    Creating a Twitter clone

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,7 +1,16 @@
 import "./index.css";
 
 const Footer = () => {
-  return <div className="Footer">Footer</div>;
+  return (
+    <div className="Footer">
+      <ul className="Footer__content">
+        <li className="home"></li>
+        <li className="search"></li>
+        <li className="notification"></li>
+        <li className="messages"></li>
+      </ul>
+    </div>
+  );
 };
 
 export default Footer;
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,7 +1,25 @@
 import "./index.css";
 
 const Navbar = () => {
-  return <div className="Navbar"> Navbar</div>;
+  return (
+    <div className="Navbar">
+      <div className="hamburger_menu">
+        <div className="line"></div>
+        <div className="line"></div>
+        <div className="line"></div>
+      </div>
+      <img
+        src="https://img.icons8.com/color/512/twitter.png"
+        alt="logo_twitter"
+        className="logo"
+      />
+      <img
+        src="https://img.icons8.com/ios/01b9f5/sparkling.png"
+        alt="sparkle_logo"
+        className="sparkle_logo"
+      />
+    </div>
+  );
 };
 
 export default Navbar;
